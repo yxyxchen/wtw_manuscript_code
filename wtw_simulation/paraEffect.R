0d0327201e52cc0e24643c57c04a4dd6c832f30f
cdlab <cdlab.at.bu@gmail.com>
2022-03-22 15:02:32 -0400

    daily

--- wtw_simulation/paraEffect.R
+++ wtw_simulation/paraEffect.R
@@ -13,6 +13,17 @@ paraEffect = function(){
   # load expParas
   load("expParas.RData")
   
+  # fix the delay sequence 
+  nRep = 10
+  set.seed(123)
+  delays_ = vector(mode = "list", length = nRep)
+  for(i in 1 : nRep){
+    delays_[[i]] = list(
+      HP = replicate(120 * 60  / 2, drawSample("HP")),
+      LP = replicate(120 * 60  / 2, drawSample("LP"))
+    )
+  }
+  
   # simulate with different parameter combinations
   modelName_ = c("QL2", "RL2")
   paraLabels_ = list(
@@ -20,16 +31,45 @@ paraEffect = function(){
     c("alpha", "nu", "tau", "eta", "beta")
   )
   nCut = 4
+  # paraSamples_ = list(
+  #   list("HP" = data.frame(
+  #     alpha = seq(0.01, 0.05, length.out = nCut),
+  #     nu = seq(0.03, 3, length.out = nCut),
+  #     tau = exp(seq(log(0.5), log(8), length.out = nCut)),
+  #     gamma = seq(0.85, 0.95, length.out = nCut),
+  #     prior = seq(0, 1, length.out = nCut)
+  #   ), "LP" = data.frame(
+  #     alpha = seq(0.01, 0.05, length.out = nCut),
+  #     nu = seq(0.03, 3, length.out = nCut),
+  #     tau = exp(seq(log(0.5), log(8), length.out = nCut)),
+  #     gamma = seq(0.85, 0.95, length.out = nCut),
+  #     prior = seq(2, 6, length.out = nCut)
+  #   )),
+  #   list("HP" = data.frame(
+  #     alpha = seq(0.01, 0.05, length.out = nCut),
+  #     nu = seq(0.03, 3, length.out = nCut),
+  #     tau = exp(seq(log(0.5), log(8), length.out = nCut)),
+  #     prior = seq(0, 1, length.out = nCut),
+  #     beta = seq(0.0105, 0.025, length.out = nCut)
+  #   ), "LP" = data.frame(
+  #     alpha = seq(0.01, 0.05, length.out = nCut),
+  #     nu = seq(0.03, 3, length.out = nCut),
+  #     tau = exp(seq(log(0.5), log(8), length.out = nCut)),
+  #     prior = seq(2, 6, length.out = nCut),
+  #     beta = seq(0.0105, 0.025, length.out = nCut)
+  #   ))
+  # )
+  
   paraSamples_ = list(
     list("HP" = data.frame(
       alpha = seq(0.01, 0.05, length.out = nCut),
-      nu = seq(0.03, 3, length.out = nCut),
+      nu = c(0.25, 0.5, 1, 2),
       tau = exp(seq(log(0.5), log(8), length.out = nCut)),
       gamma = seq(0.85, 0.95, length.out = nCut),
       prior = seq(0, 1, length.out = nCut)
     ), "LP" = data.frame(
       alpha = seq(0.01, 0.05, length.out = nCut),
-      nu = seq(0.03, 3, length.out = nCut),
+      nu = c(0.25, 0.5, 1, 2),
       tau = exp(seq(log(0.5), log(8), length.out = nCut)),
       gamma = seq(0.85, 0.95, length.out = nCut),
       prior = seq(2, 6, length.out = nCut)
@@ -48,10 +88,10 @@ paraEffect = function(){
       beta = seq(0.0105, 0.025, length.out = nCut)
     ))
   )
-  
+  # loop over models
   figs_ = vector(mode = "list", length = length(modelName_))
   for(i in 1 : length(modelName_)){
-    figs_[[i]] =simPostHoc(modelName_[i], paraLabels_[[i]], paraSamples_[[i]])
+    figs_[[i]] =simPostHoc(modelName_[i], paraLabels_[[i]], paraSamples_[[i]], delays_)
   }
   
   return(figs_)
--- wtw_simulation/subFxs/averageRes.R
+++ wtw_simulation/subFxs/averageRes.R
@@ -1,99 +1,39 @@
 # average simualtion results over multiple repetitions 
-averageRes = function(HPSim_, LPSim_){
-  # check whether the number of repetitions is equal in two environments
-  stopifnot(length(HPSim_) == length(LPSim_))
-  
+averageRes = function(sim_, lower_boundaries, upper_boundaries){
   # constants 
-  stepSec = 1
-  nSim = length(HPSim_)
-  chunkBreaks = c(0, 4, 8, 12, 16) * 60
-  nBreak = length(chunkBreaks)
-  nHPStep = delayMaxs[1] / stepSec + 1 # since here we assume the interval is [ , ) yet in we need to include the end point at t = 20
-  nLPStep = delayMaxs[2] / stepSec + 1 
+  nRep = length(sim_) # number of repetitions
+  stepSec = 1 # 
+  nStep = length(sim_[[1]]$Qwaits_[,1]) # number of time steps, since here we assume the interval is [ , ) yet in we need to include the end point at t = 20
+  nBreak = length(lower_boundaries) # number of analysis time windows 
   
-  # record and average the relative value of waiting
-  if(!is_null(HPSim_[[1]]$Qwaits_)){
-    ## initialize value function outputs
-    HPRvQwaits_ = array(dim = c(nHPStep, nBreak, nSim))
-    LPRvQwaits_ = array(dim = c(nLPStep, nBreak, nSim))
-    asymHPRvQwaits_ = matrix(NA, nHPStep, nSim)
-    asymLPRvQwaits_ = matrix(NA, nLPStep, nSim)
-    ## record values 
-    for(i in 1 : nSim){
-      HPSim = HPSim_[[i]]
-      LPSim = LPSim_[[i]]
-      for(j in 1 : nBreak){
-        HPRvQwaits_[ , j, i] = HPSim$Qwaits_[1 : nHPStep, which.min(abs(HPSim$sellTime - chunkBreaks[j]))] - HPSim$V_[which.min(abs(HPSim$sellTime - chunkBreaks[j]))]
-        LPRvQwaits_[ , j, i] = LPSim$Qwaits_[1 : nLPStep, which.min(abs(LPSim$sellTime - chunkBreaks[j]))] - LPSim$V_[which.min(abs(LPSim$sellTime - chunkBreaks[j]))]
-      }
-      asymHPRvQwaits_[,i] = HPSim$Qwaits_[ , ncol(HPSim$Qwaits_)] - HPSim$V_[ncol(HPSim$Qwaits_)]
-      asymLPRvQwaits_[,i] = LPSim$Qwaits_[ , ncol(LPSim$Qwaits_)] - LPSim$V_[ncol(LPSim$Qwaits_)]
+  # initialize the outputs
+  wait_reps = array(dim = c(nStep, nBreak, nRep))
+  quit_reps = matrix(NA, nrow = nBreak, ncol = nRep)
+  wait_minus_quit_reps = array(dim = c(nStep, nBreak, nRep))
+  sub_auc_reps = matrix(NA, nrow = nBreak, ncol = nRep)
+  # loop over reps
+  for(i in 1 : nRep){
+    for(j in 1 : nBreak){
+      sim = sim_[[i]]
+      wait_reps[, j, i] = sim$Qwaits_[, which.min(abs(sim$sellTime - upper_boundaries[j]))]
+      quit_reps[j, i] = sim$V_[which.min(abs(sim$sellTime - upper_boundaries[j]))] 
+      wait_minus_quit_reps[ , j, i] =wait_reps[, j, i]  - quit_reps[j, i] 
+      sim$Qwaits_ = NULL
+      sim$Gs_ = NULL
+      sim = as.data.frame(sim)
+      sub_auc_reps[j,i] = kmsc(sim[lower_boundaries[j] < sim$sellTime & sim$sellTime <= upper_boundaries[j],], min(delayMaxs), F, kmGrid)$auc
     }
-    ## average across simulations
-    HPRvQwaits = list(
-      mu = apply(HPRvQwaits_, FUN = mean, MARGIN = c(1, 2)),
-      se = apply(HPRvQwaits_, FUN = calc_se, MARGIN = c(1, 2))
-    )
-    LPRvQwaits = list(
-      mu = apply(LPRvQwaits_, FUN = mean, MARGIN = c(1, 2)),
-      se = apply(LPRvQwaits_, FUN = calc_se, MARGIN = c(1, 2))
-    )
-    asymHPRvQwaits = list(mu = apply(asymHPRvQwaits_, FUN = mean, MARGIN = 1), se = apply(asymHPRvQwaits_, FUN = calc_se, MARGIN = 1))
-    asymLPRvQwaits = list(mu = apply(asymLPRvQwaits_, FUN = mean, MARGIN = 1), se = apply(asymLPRvQwaits_, FUN = calc_se, MARGIN = 1))
   }
-
-  
-  # record and average AUC
-  # initialize auc outputs
-  HPaucs_ = matrix(NA, nrow = nBreak - 1, ncol = nSim)
-  LPaucs_ = matrix(NA, nrow = nBreak - 1, ncol = nSim)
-  asymHPauc_ = vector(length = nSim)
-  asymLPauc_ = vector(length = nSim)
-  for(i in 1 : nSim){
-    HPSim = HPSim_[[i]]
-    HPSim$Qwaits_ = NULL
-    HPSim$Gs_ = NULL
-    HPSim = as.data.frame(HPSim)
-    HPaucs_[,i] =  sapply(1 : (nBreak - 1), function(x) kmsc(HPSim[(x-1) * 4 * 60 <= HPSim$sellTime & HPSim$sellTime < x * 4 * 60,], min(delayMaxs), F, kmGrid)$auc)
-    asymHPauc_[i] = kmsc(HPSim[116 * 60 <= HPSim$sellTime,], min(delayMaxs), F, kmGrid)$auc
-      
-    LPSim = LPSim_[[i]]
-    LPSim$Qwaits_ = NULL
-    LPSim$Gs_ = NULL
-    LPSim = as.data.frame(LPSim)
-    LPaucs_[,i] = sapply(1 : (nBreak - 1), function(x) kmsc(LPSim[(x-1) * 4 * 60 <= LPSim$sellTime & LPSim$sellTime < x * 4 * 60,], min(delayMaxs), F, kmGrid)$auc)
-    asymLPauc_[i] = kmsc(LPSim[116 * 60 <= LPSim$sellTime,], min(delayMaxs), F, kmGrid)$auc
-  }
-  HPaucs = list(
-    mu = apply(HPaucs_, FUN = mean, MARGIN = 1), se = apply(HPaucs_, FUN = calc_se, MARGIN = 1)
-  )
-  LPaucs = list(
-    mu = apply(LPaucs_, FUN = mean, MARGIN = 1), se = apply(LPaucs_, FUN = calc_se, MARGIN = 1)
-  )
-  
-  asymHPauc = list(mu = mean(asymHPauc_), se = calc_se(asymHPauc_))
-  asymLPauc = list(mu = mean(asymLPauc_), se = calc_se(asymLPauc_))
-  
-  # return the output
- if(!is_null(HPSim_[[1]]$Qwaits_)){
-   output = list(
-     HPRvQwaits = HPRvQwaits,
-     LPRvQwaits = LPRvQwaits,
-     asymHPRvQwaits = asymHPRvQwaits,
-     asymLPRvQwaits = asymLPRvQwaits,
-     HPaucs = HPaucs,
-     LPaucs = LPaucs,
-     asymHPauc = asymHPauc,
-     asymLPauc = asymLPauc
+  wait_minus_quit_ = apply(wait_minus_quit_reps, MARGIN = c(1,2), FUN = mean)
+  sub_auc_ = apply(sub_auc_reps, MARGIN = 1, FUN = mean)
+  wait_ = apply(wait_reps, MARGIN = c(1,2), FUN = mean)
+  quit_ = apply(quit_reps, MARGIN = 1, FUN = mean)
+  output = list(
+     sub_auc_ = sub_auc_,
+     wait_minus_quit_ = wait_minus_quit_,
+     wait_ = wait_,
+     quit_ = quit_
    )
- }else{
-   output = list(
-     HPaucs = HPaucs,
-     LPaucs = LPaucs,
-     asymHPauc = asymHPauc,
-     asymLPauc = asymLPauc
-   )
- }
   return(output)
 }
   
