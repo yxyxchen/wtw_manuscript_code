c2512e0fc408fa1675c45645977f2a8f40a65deb
cdlab <cdlab.at.bu@gmail.com>
2022-04-11 18:05:12 -0400

    daily

--- wtw_comparemodel/modelComparision.m
+++ wtw_comparemodel/modelComparision.m
@@ -1,49 +1,49 @@
 
 nExp = 3;
+% 
+% % compare the four variants of the RL model
+% EF4_ = zeros([3, 4]); % expeted model frequencies 
+% PXP4_ = zeros([3, 4]); % protected prob of exceedance 
+% for i = 1 : nExp
+% 	fileDir = sprintf('../../genData/wtw_exp%d/waic.csv', i);
+%     % fileDir = sprintf('waic_exp%d.csv', i);
+% 	waic = csvread(fileDir);
+% 	
+% 	% transfer into the loss domain 
+% 	L = -waic';
+% 
+% 	% exclude participants with disconvergent model fitting results 
+% 	L = L(1:4, all(L(7:10,:)));
+% 
+% 	% run the group-level Bayesian model selection, Stephan et al., 2009 
+% 	[posterior,out] = VBA_groupBMC(L);
+% 	f  = out.Ef;
+% 	EP = out.ep;
+% 	EF4_(i, :)  = out.Ef;
+% 	PXP4_(i, :) = out.ep;
+% end 
+% csvwrite('../../genData/EF4.csv', EF4_, 3, 4);
+% csvwrite('../../genData/PXP4.csv', PXP4_, 3, 4);
 
-% compare the four variants of the RL model
-EF4_ = zeros([3, 4]); % expeted model frequencies 
-PXP4_ = zeros([3, 4]); % protected prob of exceedance 
+% add the non-learning benchmark model
+EF5_ = zeros([3, 5]); % expeted model frequencies 
+PXP5_ = zeros([3, 5]); % protected prob of exceedance 
 for i = 1 : nExp
 	fileDir = sprintf('../../genData/wtw_exp%d/waic.csv', i);
-    % fileDir = sprintf('waic_exp%d.csv', i);
 	waic = csvread(fileDir);
 	
 	% transfer into the loss domain 
 	L = -waic';
 
 	% exclude participants with disconvergent model fitting results 
-	L = L(1:4, all(L(7:10,:)));
+	L = L(1:5, all(L(7:11,:)));
 
 	% run the group-level Bayesian model selection, Stephan et al., 2009 
 	[posterior,out] = VBA_groupBMC(L);
 	f  = out.Ef;
 	EP = out.ep;
-	EF4_(i, :)  = out.Ef;
-	PXP4_(i, :) = out.ep;
+	EF5_(i, :)  = out.Ef;
+	PXP5_(i, :) = out.ep;
 end 
-csvwrite('../../genData/EF4.csv', EF4_, 3, 4);
-csvwrite('../../genData/PXP4.csv', PXP4_, 3, 4);
-
-% add the two non-learning benchmark models 
-EF6_ = zeros([3, 6]); % expeted model frequencies 
-PXP6_ = zeros([3, 6]); % protected prob of exceedance 
-for i = 1 : nExp
-	fileDir = sprintf('../../genData/wtw_exp%d/waic.csv', i);
-	waic = csvread(fileDir);
-	
-	% transfer into the loss domain 
-	L = -waic';
-
-	% exclude participants with disconvergent model fitting results 
-	L = L(1:6, all(L(7:12,:)));
-
-	% run the group-level Bayesian model selection, Stephan et al., 2009 
-	[posterior,out] = VBA_groupBMC(L);
-	f  = out.Ef;
-	EP = out.ep;
-	EF6_(i, :)  = out.Ef;
-	PXP6_(i, :) = out.ep;
-end 
-csvwrite('../../genData/EF6.csv', EF6_, 3, 6);
-csvwrite('../../genData/PXP6.csv', PXP6_, 3, 6);
+csvwrite('../../genData/EF5.csv', EF5_, 3, 5);
+csvwrite('../../genData/PXP5.csv', PXP5_, 3, 5);
